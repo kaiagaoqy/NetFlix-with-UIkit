845afe3b28634b16970d619835a599f8e0204a5f
kaia <kaiagaoqy@gmail.com>
2022-11-24 13:07:48 -0500

    Update Home View Controller

---
+++ NetFlix_UIKit/CollectionViewTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  CollectionViewTableViewCell.swift
+//  NetFlix_UIKit
+//
+//  Created by Kaia Gao on 11/24/22.
+//
+
+import UIKit
+
+class CollectionViewTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
