2762b56c3102436a85cdda0b73d8903e00aef9fe
kaia <kaiagaoqy@gmail.com>
2022-11-24 19:56:45 -0500

    Update Header and navbar on Home View

---
+++ NetFlix_UIKit/HeaderUIView.swift
@@ -0,0 +1,20 @@
+//
+//  HeaderUIView.swift
+//  NetFlix_UIKit
+//
+//  Created by Kaia Gao on 11/24/22.
+//
+
+import UIKit
+
+class HeaderUIView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
